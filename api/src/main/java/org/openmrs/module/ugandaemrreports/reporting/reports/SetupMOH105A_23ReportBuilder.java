dd8a2343eb8fdae2a5bebd6816edbb5c82da7662
ningosi <nicotwendelee@gmail.com>
2017-05-29 12:34:16 +0300

    initial work on 105A section 2.3

---
+++ api/src/main/java/org/openmrs/module/ugandaemrreports/reporting/reports/SetupMOH105A_23ReportBuilder.java
@@ -0,0 +1,109 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.ugandaemrreports.reporting.reports;
+
+import org.openmrs.module.reporting.dataset.definition.CohortIndicatorDataSetDefinition;
+import org.openmrs.module.reporting.evaluation.parameter.Mapped;
+import org.openmrs.module.reporting.evaluation.parameter.Parameter;
+import org.openmrs.module.reporting.report.ReportDesign;
+import org.openmrs.module.reporting.report.definition.ReportDefinition;
+import org.openmrs.module.ugandaemrreports.library.Moh105IndicatorLibrary;
+import org.openmrs.module.ugandaemrreports.reporting.library.dimension.CommonReportDimensionLibrary;
+import org.openmrs.module.ugandaemrreports.reporting.utils.ReportUtils;
+import org.openmrs.module.ugandaemrreports.reports.UgandaEMRDataExportManager;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by Nicholas Ingosi on 5/29/17.
+ */
+public class SetupMOH105A_23ReportBuilder extends UgandaEMRDataExportManager {
+
+    @Autowired
+    private CommonReportDimensionLibrary dimensionLibrary;
+
+    @Autowired
+    private Moh105IndicatorLibrary indicatorLibrary;
+
+    /**
+     * @return the uuid for the report design for exporting to Excel
+     */
+    @Override
+    public String getExcelDesignUuid() {
+        return "3f07996a-444c-11e7-9010-507b9dc4c741";
+    }
+
+    /**
+     * Build the report design for the specified report, this allows a user to override the report design by adding properties and other metadata to the report design
+     *
+     * @param reportDefinition
+     * @return The report design
+     */
+    @Override
+    public ReportDesign buildReportDesign(ReportDefinition reportDefinition) {
+        return createExcelTemplateDesign(getExcelDesignUuid(), reportDefinition, "105PostnatalReport.xls");
+    }
+
+    @Override
+    public String getUuid() {
+        return "4e2ff23e-444c-11e7-ae36-507b9dc4c741";
+    }
+
+    @Override
+    public String getName() {
+        return "HMIS 105 MCH - POSTNATAL";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Health Unit Outpatient Monthly Report, Maternal and Child Health 2.3 Postnatal";
+    }
+
+    @Override
+    public ReportDefinition constructReportDefinition() {
+
+        ReportDefinition rd = new ReportDefinition();
+        rd.setUuid(getUuid());
+        rd.setName(getName());
+        rd.setDescription(getDescription());
+        rd.setParameters(getParameters());
+
+
+
+        CohortIndicatorDataSetDefinition dsd = new CohortIndicatorDataSetDefinition();
+        dsd.setParameters(getParameters());
+        dsd.addDimension("age", ReportUtils.map(dimensionLibrary.standardAgeGroupsForAnc(), "onDate=${endDate}"));
+
+
+        //connect the report definition to the dsd
+        rd.addDataSetDefinition("2.3-indicators", Mapped.mapStraightThrough(dsd));
+        return rd;
+    }
+
+    @Override
+    public String getVersion() {
+        return "0.1";
+    }
+
+    @Override
+    public List<Parameter> getParameters() {
+        List<Parameter> l = new ArrayList<Parameter>();
+        l.add(new Parameter("startDate", "Start Date", Date.class));
+        l.add(new Parameter("endDate", "End Date", Date.class));
+        return l;
+    }
+}
